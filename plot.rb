842173bbfc2e93c7ac6a34bfb3b931a71379044c
Hugo Corbucci <hugo.corbucci@gmail.com>
2010-02-19 16:37:06 -0200

    Adding some data from our repository. Improving a bit the text.
    Hugo

---
+++ plot.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'gnuplot'
+
+def plot_info arquivo_de_dados, indice, nome
+  Gnuplot::DataSet.new( "\"#{arquivo_de_dados}\"" ) do |ds|
+    ds.using = "1:#{indice}"
+    ds.with = 'lines'
+    ds.linewidth = 3
+    ds.title = nome
+  end
+end
+
+def plot nome_imagem, arquivo_de_dados, colunas, tamanho = "800, 400"
+  Gnuplot.open do |gp|
+    Gnuplot::Plot.new( gp ) do |plot|
+      plot.set "terminal png size #{tamanho}"
+      plot.set 'output', "#{nome_imagem}.png"
+      plot.title  nome_imagem
+      plot.set 'nogrid'
+      plot.set 'xdata', 'time'
+      plot.set 'timefmt', '"%d-%m-%Y"'
+      plot.xrange "[\"03-07-2009\":\"18-02-2010\"]"
+      plot.set 'xtics', "nomirror \"03-07-2009\", 2419200, \"18-02-2010\""
+      plot.yrange "[0:50000]"
+
+      colunas.each_with_index do |coluna, indice|
+        plot.data << plot_info(arquivo_de_dados, indice+2, coluna)
+      end
+    end
+  end
+end
+
+grafico = ARGV[0]
+nome_arquivo = ARGV[1]
+colunas = ARGV[2..-1]
+
+if File.exists? nome_arquivo
+  plot grafico, nome_arquivo, colunas
+else
+  puts "Arquivo passado não existe"
+end
+
---
+++ refactoring.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'gnuplot'
+
+def plot nome_imagem, arquivo_de_dados, tamanho = "400, 400"
+  Gnuplot.open do |gp|
+    Gnuplot::Plot.new( gp ) do |plot|
+      plot.set "terminal png size #{tamanho}"
+      plot.set 'output', "#{nome_imagem}.png"
+      plot.title  "Refactoring time per iteration"
+      plot.set 'nogrid'
+      plot.xrange "[1:20]"
+      plot.yrange "[0:40]"
+
+      plot.data << Gnuplot::DataSet.new( "\"#{arquivo_de_dados}\"" ) do |ds|
+        ds.using = '1:2'
+        ds.with = 'lines'
+        ds.linewidth = 3
+        ds.title = 'Refactoring spent time'
+      end
+    end
+  end
+end
+
+grafico = "refactoring"
+nome_arquivo = "#{grafico}.txt"
+plot(grafico, nome_arquivo) if(File.exists? nome_arquivo)
+
